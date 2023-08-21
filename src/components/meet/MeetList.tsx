839dec9b7a99dad4219e38090da3a89cb90c918d
Gustavo Pina <115650988+GuPina@users.noreply.github.com>
2023-08-21 17:21:24 -0300

    Aula 05 Parte 04

--- src/components/meet/MeetList.tsx
+++ src/components/meet/MeetList.tsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react"
 import emptyIcon from '../../assets/images/empty_list.svg';
 import { MeetServices } from "../../services/MeetServices";
 import { MeetListItem } from "./MeetListItem";
+import { Modal } from "react-bootstrap";
 
 const meetServices = new MeetServices();
 
 export const MeetList = () => {
 
     const [meets, setMeets] = useState([]);
+    const [ShowModal, setShowModal] = useState(false);
+    const [selected, setSelected] = useState<String | null >(null);
 
     const getMeets = async () => {
         try {
@@ -20,15 +23,37 @@ export const MeetList = () => {
         }
     }
 
-    const selectToRemove = () => {
+    const selectToRemove = (id: string) => {
+        setSelected(id);
+        setShowModal(true);
+    }
 
+    const cancelSelection = () => {
+        setSelected(null);
+        setShowModal(false);
     }
 
     useEffect(() => {
         getMeets();
     }, []);
 
+    const removeMeet = async() =>{
+        try {
+            const result = await meetServices.getMeets();
+            if(!selected) {
+                return;
+            }
+
+            await meetServices.deleteMeet(selected);
+            await getMeets();
+            cancelSelection();
+        } catch (e) {
+            console.log('Ocorreu erro ao excluir reunião:', e )
+        }
+    }
+
     return (
+        <>
         <div className="container-meet-list">
             {meets && meets.length > 0
                 ?
@@ -40,5 +65,26 @@ export const MeetList = () => {
                     </div>
                 }
         </div>
+        <Modal
+            show={ShowModal}
+            onHide={() => setShowModal(false)}
+            className="container-modal">
+                <Modal.Body>
+                    <div className="content">
+                        <div className="container">
+                        <span>Deletar Reunião</span>
+                        <p>Deseja deletar reunião?</p>
+                        <p>Essa ação não poderá ser desfeita.</p>
+                        </div>
+                        <div className="actions">
+                            <span onClick={cancelSelection}>Cancelar</span>
+                            <button onClick={removeMeet}>Confirmar</button>
+
+                        </div>
+                    </div>
+                </Modal.Body>
+
+        </Modal>
+        </>
     )
 }
