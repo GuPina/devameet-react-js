419a186aa3440b6c9840d77434f0fc9f4e21d4d5
Gustavo Pina <115650988+GuPina@users.noreply.github.com>
2023-10-24 18:37:04 -0300

    Revert

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,9 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import { Login } from "../views/Login";
 import { Register } from "../views/Register";
 import { Home } from "../views/Home";
+<<<<<<< HEAD
 import { Profile } from "../views/Profile";
 import { MeetAddView } from "../views/MeetAdd";
 import { MeetEditView } from "../views/MeetEdit";
+<<<<<<< HEAD
+=======
+import { LinkView } from "../views/Link";
+import { RoomView } from "../views/Room";
+=======
+>>>>>>> parent of 6194cba (Aula 04 Parte 01)
+>>>>>>> parent of eb4845c (Revert "Aula 07 Parte 03")
 
 
 export const getRouter = (token: string) => {
@@ -28,12 +36,21 @@ export const getRouter = (token: string) => {
                 path:'*',
                 id:'login',
                 element: <Home/>
+<<<<<<< HEAD
             },
             {
                 path: '/user',
                 id: 'user',
                 element: <Profile />
+            },
+            {
+                path: '/room/:link',
+                id: 'room',
+                element: <RoomView />
+=======
+>>>>>>> parent of 6194cba (Aula 04 Parte 01)
             }
+            
         ];
 
         const mobile = window.innerWidth <= 992;
---
+++ src/services/RoomServices.ts
@@ -0,0 +1,8 @@
+import { HttpApiServices } from "./HttpApiServices";
+
+
+export class RoomServices extends HttpApiServices {
+    getRoomByLink(link:string){
+        return this.get('/room/'+link);
+    }
+}
